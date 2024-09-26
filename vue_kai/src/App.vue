156e4aa3db373ece2bcce7701f6ee046bb3e4e08
kai114615 <peter82615@yahoo.com.tw>
2024-09-26 20:29:05 +0800

    20240926-新增Vue專案

---
+++ vue_kai/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import
+</script>
+
+<template>
+  <h2>kai</h2>
+  <hr>
+
+</template>
+
+<style scoped>
+.h2{
+  color: aqua;
+}
+</style>
---
+++ vue_kai/src/components/FirstComponment.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <h2>first com</h2>
+  <hr>
+
+</template>
+
+<style scoped>
+h2{
+  color: red;
+}
+</style>
